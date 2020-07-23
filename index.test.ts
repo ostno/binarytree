7b62875268b8a8460a1e2b78ee3274b0ae4fba80
ostno <ost.noel@gmail.com>
2020-07-23 12:03:44 +0200

    Working version with some simple test

--- index.test.ts
+++ index.test.ts
@@ -1,17 +1,91 @@
-import { Game } from "./index";
+import { Node } from "./index";
 
-describe('game', () => {
+describe('binary tree', () => {
 
-    let game: Game;
+    function createSimpleTree(): Node {
+        const node = new Node(2);
+        node.insert(1);
+        node.insert(3);
+        node.insert(4);
+        return node;
+    }
 
-    beforeEach(() => {
-        game = new Game();
+    function createComplexTree(): Node {
+        const node = new Node(5);
+        node.insert(1);
+        node.insert(6);
+        node.insert(3);
+        node.insert(4);
+        node.insert(9);
+        return node;
+    }
+
+    describe('single node', () => {
+
+        test('single node should return 1', () => {
+            const node = new Node(1);
+            expect(node.value).toEqual(1);
+            expect(node.left).not.toBeDefined();
+            expect(node.right).not.toBeDefined();
+        });
     });
 
-    describe('test', () => {
-        test('should be defined', () => {
-		expect(game).toBeDefined();
+    describe('node insertion', () => {
+
+        test('greater than is on the right', () => {
+            const node = new Node(2);
+            node.insert(3);
+            expect(node.right.value).toEqual(3);
         });
+
+        test('less than is on the left', () => {
+            const node = new Node(2);
+            node.insert(1);
+            expect(node.left.value).toEqual(1);
+        });
+
+        test('a fourth node greater than any other should be on the right', () => {
+            const node = createSimpleTree();
+            expect(node.right.right.value).toEqual(4)
+        });
+
+    });
+
+    describe('flatten', () => {
+
+        test('should returns an array of values (simple tree)', () => {
+            const node = createSimpleTree();
+            const result = node.flatten();
+            expect(result).toEqual([2, 1, 3, 4]);
+        });
+
+        test('should returns an array of values (complex tree)', () => {
+            const node = createComplexTree();
+            const result = node.flatten();
+            expect(result).toEqual([5, 1, 3, 4, 6, 9]);
+        });
+
+    });
+
+    describe('double', () => {
+
+        test('should double the value for each node', () => {
+            const node = createSimpleTree();
+            node.double();
+            const result = node.flatten();
+            expect(result).toEqual([4, 2, 6, 8]);
+        });
+
+    });
+
+    describe('sum', () => {
+
+        test('should return the sum of all nodes', () => {
+            const node = createSimpleTree();
+            const result = node.sum();
+            expect(result).toEqual(10);
+        });
+
     });
 
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,87 @@
-export class Game{
+export class Node {
+    value: number;
+    left: Node | undefined;
+    right: Node | undefined;
+
+    constructor(value: number) {
+        this.value = value;
+    }
+
+    insert(newValue: number) {
+        if (newValue > this.value) {
+            if (this.right) {
+                this.right.insert(newValue);
+            } else {
+                this.right = new Node(newValue);
+            }
+        } else {
+            if (this.left) {
+                this.left.insert(newValue);
+            } else {
+                this.left = new Node(newValue);
+            }
+        }
+    }
+
+    double() {
+        const allNodes = this.getAllNodes();
+        allNodes.map(node => node.value = node.value * 2);
+    }
+
+    sum() {
+        const allNodes = this.getAllNodes();
+        return allNodes.reduce((a: number, b: Node) => { return a + b.value }, 0);
+    }
+
+    getAllNodes(): Node[] {
+
+        let result: Node[] = [this];
+
+        const nodesWithUnexploredRightSide: Node[] = [];
+        let node: Node = this;
+        let navigation: 'left' | 'right' = 'left';
+
+        while (node) {
+
+            navigation = this.getNavigation(node);
+
+            if (navigation === 'left' && node.right) {
+                // Add this node to get back to it and explore its right side later
+                nodesWithUnexploredRightSide.push(node);
+            }
+
+            if (node[navigation]) {
+                node = node[navigation];
+            } else {
+                node = nodesWithUnexploredRightSide[nodesWithUnexploredRightSide.length - 1].right;
+                nodesWithUnexploredRightSide.pop();
+            }
+
+            result.push(node);
+
+            if (nodesWithUnexploredRightSide.length === 0 && !node.right) {
+                break;
+            }
+
+        }
+
+        return result;
+
+    }
+
+    flatten(): number[] {
+
+        const result = [];
+        let allNodes = this.getAllNodes();
+        allNodes.forEach((node) => {
+            result.push(node.value);
+        });
+        return result;
+
+    }
+
+    getNavigation(node: Node): 'right' | 'left' {
+        return (!node.left && node.right) ? 'right' : 'left';
+    }
 
 }
